c298a5f68e506a08a17d4af3ea500ab2fda768c2
ocelperf <LaTextXyuMe@users.noreply.github.com>
2023-01-31 00:57:01 +0900

    Polygon Class Added

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include "polygon.hpp"
 
 int main() 
 {
-    std::cout << "Hello My Ai!" << std::endl;
+    Polygon rectangle;
+    std::cout << "Area of rectangle is: " << rectangle.getArea(2,4);
+    std::cout << "\n Hello My Ai!" << std::endl;
     return 0;
 }
