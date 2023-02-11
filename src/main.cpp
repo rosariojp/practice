ae52347a22a0aef1c505b028e2c27b7efb956a65
ocelperf <LaTextXyuMe@users.noreply.github.com>
2023-02-12 00:54:56 +0900

    Added Rectangle Class

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 #include "polygon.hpp"
+#include "rectangle.hpp"
 
 int main() 
 {
     int height = 2;
     int width = 4;
-    Polygon rectangle(height, width);
+    int length = 6;
+    Polygon polygon(height, width);
+    Rectangle rectangle(length, width);
 
-    std::cout << "Height: " << rectangle.getHeight() << std::endl;
-    std::cout << "Width: " << rectangle.getWidth() << std::endl;
-    std::cout << "Area of rectangle is: " << rectangle.getArea();
+    std::cout << "Height: " << polygon.getHeight() << std::endl;
+    std::cout << "Width: " << polygon.getWidth() << std::endl;
+    std::cout << "Area of rectangle is: " << polygon.getArea() << std::endl;
+
+    std::cout << "Rectangle Length: " << rectangle.getLength() << std::endl;
+    std::cout << "Rectangle Width: " << rectangle.getWidth() << std::endl;
+    std::cout << "Area of rectangle is: " << rectangle.getArea() << std::endl;
+
+
     return 0;
 }
